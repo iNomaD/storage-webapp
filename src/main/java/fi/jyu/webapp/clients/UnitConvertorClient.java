8a63cd0a6b307ba2a5b16439ac8351511214afce
Михаил <mikhail.mingaleev@2012.auditory.ru>
2017-09-16 16:04:51 +0300

    First trying for commit in Git. UnitConvertorClient need double value, str value about income Computer Unit and outcome Computer Unit. Try use Bite, Byte,Kilobyte,Megabyte,Gigabyte,Terabyte.  Service have a little problems with convert little values to very big. And client have not got a defence from wrong input. I add it slightly later...

--- src/main/java/fi/jyu/webapp/clients/UnitConvertorClient.java
+++ src/main/java/fi/jyu/webapp/clients/UnitConvertorClient.java
@@ -1,4 +1,23 @@
 package fi.jyu.webapp.clients;
 
+import fi.jyu.webapp.clients.webservicex.ComputerUnit;
+import fi.jyu.webapp.clients.webservicex.ComputerUnitSoap;
+import fi.jyu.webapp.clients.webservicex.Computers;
+
 public class UnitConvertorClient {
+
+    public UnitConvertorClient(){}
+
+    public double sendRequest(double inputValue, String inputUnit, String outputUnit){
+
+        ComputerUnit convertorService = new ComputerUnit();
+        ComputerUnitSoap convertorServiceSoap = convertorService.getComputerUnitSoap();
+
+        Computers INUnit = Computers.fromValue(inputUnit);
+        Computers OUTUnit = Computers.fromValue(outputUnit);
+        //switch (inputUnit.toLowerCase())
+        //    case "byte":
+
+        return convertorServiceSoap.changeComputerUnit(inputValue,INUnit,OUTUnit);
+    }
 }
