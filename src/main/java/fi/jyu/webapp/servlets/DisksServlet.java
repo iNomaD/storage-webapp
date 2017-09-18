d4440cc560b9789d01e142458925159a6ea3e851
Denis <royalruby@yandex.ru>
2017-09-18 13:48:57 +0300

    output

--- src/main/java/fi/jyu/webapp/servlets/DisksServlet.java
+++ src/main/java/fi/jyu/webapp/servlets/DisksServlet.java
@@ -10,6 +10,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.List;
 
 @WebServlet("/Disks")
@@ -18,18 +19,21 @@ public class DisksServlet extends HttpServlet {
     private Processing processing = new Processing();
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
-        /*
-        processing.getDisks();;
-        processing.getCurrency();
-        processing.getGBtoMB();
-        PrintWriter out = response.getWriter();
-        out.println(processing.processXML());
-        */
+        String price = request.getParameter("price");
+        String capacity = request.getParameter("capacity");
 
         List<DiskModel> data = RequestProcessor.getStorageData();
         for(DiskModel disk : data){
             System.out.println(disk);
         }
+
+        String result = "<Result>";
+        for(DiskModel disk : data){
+            result += disk.toXML();
+        }
+        result = result + "</Result>";
+        PrintWriter out = response.getWriter();
+        out.println(result);
     }
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
