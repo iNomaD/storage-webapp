b3290b197791689695978c9c7716e12af6209bbf
svzj95 <svzj95@gmail.com>
2017-09-18 13:09:33 +0300

    Update DiskModel Method fromXML()

--- src/main/java/fi/jyu/soapservice/src/models/DiskModel.java
+++ src/main/java/fi/jyu/soapservice/src/models/DiskModel.java
@@ -1,5 +1,17 @@
 package fi.jyu.soapservice.src.models;
 
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.IOException;
+import java.io.StringReader;
+
 public class DiskModel {
 
     Integer id;
@@ -32,7 +44,30 @@ public class DiskModel {
 
     public static DiskModel fromXML(String xmlData){
         DiskModel disk = new DiskModel();
-
+        DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+        DocumentBuilder db = null;
+        try {
+            db = dbf.newDocumentBuilder();
+        } catch (ParserConfigurationException e) {
+            e.printStackTrace();
+        }
+        Document doc = null;
+        try {
+            doc = db.parse(new InputSource(new StringReader(xmlData)));
+        } catch (SAXException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        NodeList nList = doc.getElementsByTagName("HD");
+        disk.id = Integer.valueOf(((Element) nList.item(0)).getElementsByTagName("ID").item(0).getTextContent());
+        disk.capacity = Integer.valueOf(((Element) nList.item(0)).getElementsByTagName("Capacity").item(0).getTextContent());
+        disk.price = Integer.valueOf(((Element) nList.item(0)).getElementsByTagName("Price").item(0).getTextContent());
+        disk.rpm = Integer.valueOf(((Element) nList.item(0)).getElementsByTagName("Rpm").item(0).getTextContent());
+        disk.vendor = ((Element) nList.item(0)).getElementsByTagName("Vendor").item(0).getTextContent();
+        disk.type = ((Element) nList.item(0)).getElementsByTagName("Type").item(0).getTextContent();
+        disk.capacityUnits = ((Element) nList.item(0)).getElementsByTagName("CapacityUnits").item(0).getTextContent();
+        disk.priceUnits = ((Element) nList.item(0)).getElementsByTagName("priceUnits").item(0).getTextContent();
         return disk;
     }
 
