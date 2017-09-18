340becac6489e81fda7f11d4af6f342f7b180a64
Denis <royalruby@yandex.ru>
2017-09-18 13:59:26 +0300

    Price convertor

--- src/main/java/fi/jyu/webapp/servlets/DisksServlet.java
+++ src/main/java/fi/jyu/webapp/servlets/DisksServlet.java
@@ -1,16 +1,20 @@
 package fi.jyu.webapp.servlets;
 
+import fi.jyu.soapservice.src.models.Constants;
 import fi.jyu.soapservice.src.models.DiskModel;
 import fi.jyu.webapp.Processing;
 import fi.jyu.webapp.RequestProcessor;
+import fi.jyu.webapp.clients.CurrencyConvertorClient;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.xml.datatype.DatatypeConfigurationException;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.text.ParseException;
 import java.util.List;
 
 @WebServlet("/Disks")
@@ -24,6 +28,21 @@ public class DisksServlet extends HttpServlet {
 
         List<DiskModel> data = RequestProcessor.getStorageData();
         for(DiskModel disk : data){
+            if(price != null && price.equalsIgnoreCase(Constants.priceEUR)){
+                CurrencyConvertorClient cclient = new CurrencyConvertorClient();
+                try {
+                    double eur = cclient.sendRequest(disk.getPrice(), Constants.priceUSD);
+                    disk.setPrice((int)eur);
+                    disk.setPriceUnits(Constants.priceEUR);
+                }
+                catch (ParseException | DatatypeConfigurationException e) {
+                    e.printStackTrace();
+                }
+            }
+            if(capacity != null && capacity.equalsIgnoreCase(Constants.capacityMB)){
+
+            }
+
             System.out.println(disk);
         }
 
