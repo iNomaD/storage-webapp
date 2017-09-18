c0beb3ae0dbe6195c81608eadfc347ca09d05813
Denis <royalruby@yandex.ru>
2017-09-18 13:11:19 +0300

    XML parser

--- src/main/java/fi/jyu/webapp/RequestProcessor.java
+++ src/main/java/fi/jyu/webapp/RequestProcessor.java
@@ -1,5 +1,53 @@
 package fi.jyu.webapp;
 
-public class RequestProcessor {
+import fi.jyu.soapservice.src.models.DiskModel;
+import fi.jyu.webapp.clients.StorageClient;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.InputSource;
 
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import java.io.StringReader;
+import java.util.ArrayList;
+import java.util.List;
+
+public class RequestProcessor {
+    
+    public static String getXmlData(){
+        StorageClient storageClient = new StorageClient();
+        String xmlData = storageClient.GetList();
+        return xmlData;
+    }
+
+    public static Document loadXMLFromString(String xml) throws Exception
+    {
+        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder builder = factory.newDocumentBuilder();
+        InputSource is = new InputSource(new StringReader(xml));
+        return builder.parse(is);
+    }
+    
+    public static List<DiskModel> getStorageData(){
+        List<DiskModel> result = new ArrayList<>();
+        String xmlData = getXmlData();
+        DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+        try {
+            DocumentBuilder db = dbf.newDocumentBuilder();
+            Document doc = loadXMLFromString(xmlData);
+            Element root = doc.getDocumentElement();
+            NodeList nodeList = root.getElementsByTagName("HD");
+            for(int i=0; i<nodeList.getLength(); ++i){
+                Node elementNode = nodeList.item(i);
+                String elementName = elementNode.getTextContent().trim();
+                result.add(DiskModel.fromXML(elementName));
+            }
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+        return result;
+    }
 }
--- src/main/java/fi/jyu/webapp/servlets/DisksServlet.java
+++ src/main/java/fi/jyu/webapp/servlets/DisksServlet.java
@@ -1,46 +1,32 @@
 package fi.jyu.webapp.servlets;
 
+import fi.jyu.soapservice.src.models.DiskModel;
 import fi.jyu.webapp.Processing;
-import fi.jyu.webapp.clients.CurrencyConvertorClient;
-import fi.jyu.webapp.clients.StorageClient;
-import fi.jyu.webapp.clients.UnitConvertorClient;
-import org.w3c.dom.Document;
-import org.w3c.dom.Element;
-import org.w3c.dom.NodeList;
-import org.w3c.dom.ls.DOMImplementationLS;
-import org.w3c.dom.ls.LSSerializer;
-import org.xml.sax.InputSource;
-import org.xml.sax.SAXException;
-
-import javax.xml.parsers.ParserConfigurationException;
-
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.DocumentBuilder;
+import fi.jyu.webapp.RequestProcessor;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.xml.datatype.DatatypeConfigurationException;
-import javax.xml.transform.Transformer;
-import javax.xml.transform.TransformerConfigurationException;
-import javax.xml.transform.TransformerFactory;
-import javax.xml.transform.dom.DOMSource;
 import java.io.IOException;
-import java.io.PrintWriter;
-import java.io.StringReader;
-import java.text.ParseException;
+import java.util.List;
+
 @WebServlet("/Disks")
 public class DisksServlet extends HttpServlet {
 
     private Processing processing = new Processing();
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        /*
         processing.getDisks();;
         processing.getCurrency();
         processing.getGBtoMB();
         PrintWriter out = response.getWriter();
         out.println(processing.processXML());
+        */
+
+        List<DiskModel> data = RequestProcessor.getStorageData();
     }
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
